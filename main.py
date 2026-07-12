badc928bc186fece7372473bc5659c0cb035e721
Chezzey-ui <arya.ebiabdi@gmail.com>
2026-07-12 18:45:25 -0400

    Refactor main.py to include exercise tracking

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 from datetime import datetime
-import pandas
-from twilio.rest import Client
 import requests
 import os
 
-MY_API_KEY = os.environ.get("MY_API_KEY")
+today = datetime.today().strftime('%d/%m/%Y')
+current_time = datetime.now().strftime('%H:%M:%S')
 
-MY_PHONE = os.environ.get("MY_PHONE")
-MY_SEND_PHONE = os.environ.get("MY_SEND_PHONE")
-MY_ACCOUNT_SID = os.environ.get("MY_ACCOUNT_SID")
-MY_AUTH_TOKEN = os.environ.get("MY_AUTH_TOKEN")
-client = Client(MY_ACCOUNT_SID, MY_AUTH_TOKEN)
+url = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
+SHEET_ENDPOINT = os.environ.get(MY_SHEET_ENDPOINT)
 
-MY_LAT = os.environ.get("MY_LAT")
-MY_LONG = os.environ.get("MY_LONG")
+API_KEY = os.environ.get(MY_API_KEY)
+APP_ID = os.environ.get(MY_APP_ID)
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+GENDER = os.environ.get(MY_GENDER)
+AGE = os.environ.get(MY_AGE)
+WEIGHT_KG = os.environ.get(MY_WEIGHT_KG)
+HEIGHT_CM = os.environ.get(MY_HEIGHT_CM)
 
-parameters = {
-    "lat" : MY_LAT,
-    "lon": MY_LONG,
-    "appid": MY_API_KEY,
-    "cnt": 4
+headers1 = {
+    "Content-Type": "application/json",
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
 }
 
-response = requests.get(url= "https://api.openweathermap.org/data/2.5/forecast", params = parameters)
-response.raise_for_status()
-data = response.json()["list"]
-weather = [day["weather"][0]["id"] for day in data]
+data = {
+    "query" : input("What exercise did you do?: "),
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE,
+    "gender": GENDER
+}
 
+response = requests.post(url, headers = headers1, json = data)
+result = response.json()
 
-umbrella = any(num <700 for num in weather)
-if umbrella:
-    message = client.messages.create(
-        body="Bring an Umbrella!",
-        from_=f"whatsapp:{MY_SEND_PHONE}",
-        to=f"whatsapp:{MY_PHONE}",
-    )
-    print(message.status)
+BEARER_TOKEN = "AAAAAAAAAAAASNJNKJSFNJKSoek,sjndfs(1029>/s;dko2jkjads[df]sd'r=+gfdgfiji%%%slkfds"
+
+headers2 = {"Authorization": "Bearer "+BEARER_TOKEN}
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today,
+            "time": current_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(SHEET_ENDPOINT, json=sheet_inputs, headers = headers2)
+
+    print(sheet_response.text)
